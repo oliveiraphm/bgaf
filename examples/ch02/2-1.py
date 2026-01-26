3246137d122db6019343b852aec37a382ebfe99d
oliveiraphm <oliveiraphm@gmail.com>
2026-01-26 18:29:49 -0300

    until page 48

---
+++ examples/ch02/2-1.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv 
+from pathlib import Path
+
+from fastapi import FastAPI
+from openai import OpenAI
+
+load_dotenv()
+
+dotenv_path = Path(" .env")  # .env na raiz
+load_dotenv(dotenv_path)
+
+openapi_api_key = os.getenv("OPENAI_API_KEY")
+
+
+app = FastAPI()
+openai_client = OpenAI(api_key=openapi_api_key)
+
+@app.get("/")
+def root_controller():
+    return {"status": "healthy"}
+
+@app.get("/chat")
+def chat_controller(prompt: str = "Inspire me"):
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    statement = response.choices[0].message.content.strip()
+    return {"statement": statement}
