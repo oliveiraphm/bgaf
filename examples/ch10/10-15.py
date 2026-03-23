42b7a10341084214f9f946f473347e47cfcd1877
oliveiraphm <oliveiraphm@gmail.com>
2026-03-23 16:23:20 -0300

    full book

---
+++ examples/ch10/10-15.py
@@ -0,0 +1,22 @@
+from openai import AsyncOpenAI
+from pydantic import BaseModel, Field
+
+client = AsyncOpenAI()
+
+class DocumentClassification(BaseModel):
+    category: str = Field(..., description="The category of the classification")
+
+async def get_document_classification(title: str,) -> DocumentClassification | str | None:
+    response = await client.beta.chat.completions.parse(
+        model = "gpt-4o",
+        messages = [
+            {
+                "role": "system", 
+                "content": "classify the provided document into the following: ...",
+            },
+        ],
+        response_format = DocumentClassification,
+    )
+
+    message = response.choices[0].message
+    return message.parsed if message.parsed is not None else message.refusal
